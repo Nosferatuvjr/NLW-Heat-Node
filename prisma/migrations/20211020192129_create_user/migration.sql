896046e9cad2f280c9e64a1cf253ebd05a01c06f
Nosferatuvjr <vivaldochagassjr@gmail.com>
2021-10-20 18:06:03 -0300

    NLW Node Heat

---
+++ prisma/migrations/20211020192129_create_user/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "github_id" INTEGER NOT NULL,
+    "avatar_url" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "html_url" TEXT NOT NULL
+);
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import { serverHttp } from "./app";
+
+serverHttp.listen(4000, () => console.log("Server rodando na porta 4000"));
